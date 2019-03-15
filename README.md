0b798ea905f66c9c2100837f898c82133a9d0410
luis sanchez <sanchezl391@gmail.com>
2019-03-15 12:37:42 -0600

    added database config

--- README.md
+++ README.md
@@ -11,3 +11,5 @@
 2. `cd v8-bears-team-02/server`
 2. install dependencies `npm install`
 3. run the server `npm start`
+
+## Git Workflow
--- server/index.js
+++ server/index.js
@@ -1,6 +1,11 @@
 const express = require('express');
-const app = express();
+const mongoose = require('mongoose');
 const port = 5000;
+const keys = require('./config/keys');
+
+const app = express();
+
+mongoose.connect(keys.mongoURI);
 
 app.get('/', (req, res)=>{
     res.send({hello: 'there'});
