a8d0f9ddc3cdf04a19847c8cb103d1abe04b0e4e
luis sanchez <sanchezl391@gmail.com>
2019-03-15 11:26:38 -0600

    initial react client commit

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
-# v8-bears-team-02
-Add-project-description-here | Voyage-8 | https://chingu.io/
+# Stack Overflow Clone
+
+
---
+++ client/src/containers/App.jsx
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+// import { library } from '@fortawesome/fontawesome-svg-core';
+// import { faIgloo } from '@fortawesome/free-solid-svg-icons';
+// library.add(faIgloo)
+
+
+import './App.scss';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        Chingus for the win!
+      </div>
+    );
+  }
+}
+
+export default App;
