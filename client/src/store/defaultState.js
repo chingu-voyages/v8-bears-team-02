c1bbd4eca3fe62050c9198abc94d6f0d5832ab40
Al Buchala <albcf@yahoo.com>
2019-04-16 06:41:37 -0400

    Update default state to include question content

--- client/src/store/defaultState.js
+++ client/src/store/defaultState.js
@@ -14,12 +14,14 @@ export const defaultState = {
         id:123,
         category:1,
         owner: "user1",
-        title:"Why is my class failing to compile?"
+        title:"Why is my class failing to compile?",
+        content: "My class is failing to compile and I need some help."
     },{
         id:124,
         category:1,
         owner: "user1",
-        title:"How to declare a global variable?"
+        title:"How to declare a global variable?",
+        content: "The title says it all. Any help will be greatly appreciated."
     }],
     answers:[{
         id:123,
