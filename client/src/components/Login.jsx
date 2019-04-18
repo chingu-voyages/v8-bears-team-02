d425268f4ced4913b6d7d2124ce9baaad1eaf669
Al Buchala <albcf@yahoo.com>
2019-04-18 14:09:54 -0400

    Login and Register forms design v1

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -1,11 +1,111 @@
-import React from 'react';
+import React, {Fragment} from 'react';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import {Avatar, Button, FormControl, FormControlLabel, Input, InputLabel, Paper, Typography} from '@material-ui/core';
+import Header from './layout/Header';
+import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
+import withStyles from '@material-ui/core/styles/withStyles';
+
+const styles = theme => ({
+    main: {
+        width: 'auto',
+        display: 'block', // Fix IE 11 issue.
+        marginLeft: theme.spacing.unit * 3,
+        marginRight: theme.spacing.unit * 3,
+        [theme.breakpoints.up(400 + theme.spacing.unit * 3 * 2)]: {
+            width: 400,
+            marginLeft: 'auto',
+            marginRight: 'auto',
+        },
+    },
+    paper: {
+        marginTop: theme.spacing.unit * 8,
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        padding: `${theme.spacing.unit * 2}px ${theme.spacing.unit * 3}px ${theme.spacing.unit * 3}px`,
+    },
+    avatar: {
+        margin: theme.spacing.unit,
+        backgroundColor: theme.palette.secondary.main,
+    },
+    form: {
+        width: '100%', // Fix IE 11 issue.
+        marginTop: theme.spacing.unit,
+    },
+    submit: {
+        marginTop: theme.spacing.unit * 3,
+    },
+});
 
 class Login extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            username: '',
+            password: '',
+        }
+
+    }
+
+    handleChange = name => event => {
+        this.setState({
+            [name]: event.target.value,
+        });
+    };
+
     render() {
+        const {
+            username,
+            password,
+        } = this.state;
+
+        const {
+            classes
+        } = this.props;
         return(
-            <div>Login</div>
+            <Fragment>
+                <Header title="Log In" />
+                <main className={classes.main}>
+                    <CssBaseline />
+                    <Paper className={classes.paper}>
+                        <Avatar className={classes.avatar}>
+                            <LockOutlinedIcon />
+                        </Avatar>
+                        <Typography component="h1" variant="h5">
+                            Log in
+                        </Typography>
+                        <form className={classes.form}>
+                            <FormControl margin="normal" required fullWidth>
+                                <InputLabel htmlFor="email">Email Address</InputLabel>
+                                <Input
+                                    id="email"
+                                    name="email"
+                                    autoComplete="email"
+                                    autoFocus
+                                    value={this.state.username}
+                                    onChange={this.handleChange('username')}
+                                />
+                            </FormControl>
+                            <FormControl margin="normal" required fullWidth>
+                                <InputLabel htmlFor="password">Password</InputLabel>
+                                <Input name="password" type="password" id="password" autoComplete="current-password" />
+                            </FormControl>
+                            <Button
+                                type="submit"
+                                fullWidth
+                                variant="contained"
+                                color="primary"
+                                className={this.props.classes.submit}
+                            >
+                                Sign in
+                            </Button>
+                        </form>
+                    </Paper>
+                </main>
+            </Fragment>
+
         )
     }
 }
 
-export default Login;
+export default withStyles(styles)(Login);
--- client/src/components/Register.jsx
+++ client/src/components/Register.jsx
@@ -1,11 +1,135 @@
-import React from 'react';
+import React, { Fragment } from 'react';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import Header from './layout/Header';
+import {Button,Grid, Paper, TextField, Typography} from '@material-ui/core';
+import withStyles from '@material-ui/core/styles/withStyles';
+import axios from 'axios';
+
+const styles = theme => ({
+    layout: {
+        width: 'auto',
+        marginLeft: theme.spacing.unit * 2,
+        marginRight: theme.spacing.unit * 2,
+        [theme.breakpoints.up(600 + theme.spacing.unit * 2 * 2)]: {
+            width: 600,
+            marginLeft: 'auto',
+            marginRight: 'auto',
+        },
+    },
+    paper: {
+        marginTop: theme.spacing.unit * 3,
+        marginBottom: theme.spacing.unit * 3,
+        padding: theme.spacing.unit * 2,
+        [theme.breakpoints.up(600 + theme.spacing.unit * 3 * 2)]: {
+            marginTop: theme.spacing.unit * 6,
+            marginBottom: theme.spacing.unit * 6,
+            padding: theme.spacing.unit * 3,
+        },
+    },
+})
 
 class Register extends React.Component {
+    constructor(props){
+        super(props);
+
+        this.state = {
+            username: '',
+            email: '',
+            password: '',
+            messageFromServer: '',
+            showError: false,
+            registerError: false,
+            loginError: false,
+        }
+    }
+
+    handleChange = name => event => {
+        this.setState({
+            [name]: event.target.value,
+        });
+    };
+
     render(){
-        return(
-            <div>Register</div>
-        )
+        const {
+            username,
+            email,
+            password,
+            messageFromServer,
+            showError,
+            registerError,
+            loginError,
+        } = this.state;
+
+        const {
+            classes
+        } = this.props;
+
+        if(messageFromServer === ''){
+            return(
+                <Fragment>
+                    <Header title="User Registration" />
+                    <CssBaseline />
+                    <main className={classes.layout}>
+                        <Paper className={classes.paper}>
+                        <Typography variant="h6" gutterBottom>
+                            User Registration
+                        </Typography>
+                        <Grid container spacing={24}>
+                            <Grid item xs={12} md={6}>
+                                <TextField
+                                    required
+                                    id="username"
+                                    value={username}
+                                    label="User Name"
+                                    fullWidth
+                                    onChange={this.handleChange('username')}
+                                />
+                            </Grid>
+                            <Grid item xs={12} md={6}>
+                                <TextField
+                                    required
+                                    id="email"
+                                    value={email}
+                                    label="Email"
+                                    fullWidth
+                                    onChange={this.handleChange('email')}
+                                />
+                            </Grid>
+                            <Grid item xs={12} md={6}>
+                                <TextField
+                                    required
+                                    id='password'
+                                    value={password}
+                                    label="Password"
+                                    fullWidth
+                                    onChange={this.handleChange('password')}
+                                />
+                            </Grid>
+                            <Grid item xs={12} md={6}>
+                                <TextField
+                                    required
+                                    id='confirmPassword'
+                                    label="Confirm Password"
+                                    fullWidth
+                                />
+                            </Grid>
+                            <Grid item sx={12}>
+                                <Button
+                                    type="submit"
+                                    fullWidth
+                                    variant="contained"
+                                    color="primary"
+                                >
+                                    Register
+                                </Button>
+                            </Grid>
+                        </Grid>
+                        </Paper>
+                    </main>
+                </Fragment>
+            )
+        }
     }
 }
 
-export default Register;
+export default withStyles(styles)(Register);
